f45fee4bd6a431864e9e664168c8bcb87bbb9c7f
miso <miso.kucera97@gmail.com>
2020-03-20 16:41:07 +0100

    add country summary

--- src/components/CountryDetail.tsx
+++ src/components/CountryDetail.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import styles from './CountryDetail.module.scss';
 import { Chart } from './Chart';
 import { Country } from '../services/dataService';
+import {Summary, SummaryItem} from "./Summary";
 
 type Props = {
     country: Country;
 };
 
 export function CountryDetail({ country }: Props) {
+
+    const getSummary = (country: Country): SummaryItem[] => {
+        return [
+            {label: 'Confirmed', value: String(country.totalVictims.confirmed)},
+            {label: 'Recovered', value: String(country.totalVictims.recovered)},
+            {label: 'Deaths', value: String(country.totalVictims.deaths)}
+        ]
+    };
+
     return (
         <div className={styles.detail}>
             <h1>{country.name}</h1>
+            <Summary items={getSummary(country)} />
             <Chart data={country.dailyChanges} />
         </div>
     );
---
+++ src/components/Summary.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Summary.module.scss';
+
+export type SummaryItem = {
+    label: string;
+    value: string;
+};
+
+type Props = {
+    items: SummaryItem[];
+};
+
+export function Summary({ items }: Props) {
+    return (
+        <>
+            {items.length && (
+                <div className={styles.summary}>
+                    {items.map(item => (
+                        <div className={styles.item}>
+                            <p className={styles.label}>{item.label}</p>
+                            <p className={styles.value}>{item.value}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
+    );
+}
